2e6c8903157833c3875039021ace5525dcd5ff19
Undreak <steinos@pm.me>
2024-02-13 23:08:11 +0100

    added data files and data peak function

--- project.R
+++ project.R
@@ -30,20 +30,59 @@ ggplot(data, aes(x = `2_theta`)) +
   )) +
   theme_minimal()
 
-# Filter data for X values between 20 and 25
-filtered_data <- data %>%
-  filter(`2_theta` >= 20 & `2_theta` <= 25)
+# Threshold for peaks
+threshold <- 50
 
-# Find the maximum values and corresponding X values for each Y column
-max_values <- filtered_data %>%
-  summarise(
-    max_Ni75Co25 = max(Ni75Co25),
-    max_X_Ni75Co25 = `2_theta`[which.max(Ni75Co25)],
-    max_Ni50Co50 = max(Ni50Co50),
-    max_X_Ni50Co50 = `2_theta`[which.max(Ni50Co50)],
-    max_Ni25Co75 = max(Ni25Co75),
-    max_X_Ni25Co75 = `2_theta`[which.max(Ni25Co75)]
-  )
+# Filter data for peaks above the threshold
+peaks_data <- data %>%
+  filter(Ni75Co25 > threshold)
 
-# Print the results
-print(max_values)
+# Plotting peaks as bars
+ggplot(peaks_data, aes(x = `2_theta`, y = Ni75Co25)) +
+  geom_bar(
+    stat = "identity", position = "dodge",
+    color = "black", alpha = 0.7,
+    width = 0.01
+  ) +
+  ggtitle("Peaks Above Threshold in Ni75Co25") +
+  xlab("2_theta") +
+  ylab("Intensity")
+
+find_peaks <- function(data) {
+  if (length(data) < 2) {
+    return(NULL) # No peak in lists with 0 or 1 element
+  }
+  peaks <- rep(0, length(data))
+  # Create a vector to store the value and index of the peaks
+  for (i in 2:(length(data) - 1)) {
+    if (data[i] > data[i - 1] && data[i] > data[i + 1]) { # Looking for a peak
+      peaks[i] <- data[i]
+    }
+  }
+  # Checking if the first and last value is a peak or not
+  if (data[1] > data[2]) {
+    peaks[1] <- data[1]
+  }
+  if (tail(data, 1) > tail(data, 2)[1]) {
+    peaks[length(peaks)] <- data[length(data)]
+  }
+  return(peaks)
+}
+
+peaks_values <- find_peaks(peaks_data$Ni75Co25)
+print(peaks_values)
+
+data_peaks <- data.frame(
+  x = peaks_data$"2_theta",
+  y1 = peaks_data$Ni75Co25,  # Curve data
+  y2 = peaks_values  # Point data 1
+)
+
+ggplot(data_peaks, aes(x = x)) +
+  geom_line(aes(y = y1), color = "blue") +  # Curve
+  geom_point(aes(y = y2), color = "red") +  # Point series 1
+  ggtitle("Peaks Ni75Co25") +
+  xlab("2_theta") +
+  ylab("Intensity")
+
+# TODO : DO THIS BUT FOR EVERY CURVE DATA AUTOMATICALLY WITH `sapply` 
