475adcb92fbdcefd39073468f1d732c2dcf70dd8
Undreak <steinos@pm.me>
2024-02-17 15:47:30 +0100

    added Co and Ni + fix threshold + plot

--- project.R
+++ project.R
@@ -3,20 +3,26 @@ library(tidyverse)
 d1 <- read.table("AD Ni75Co25.dat")
 d2 <- read.table("AR cO50nI50.dat")
 d3 <- read.table("JGD Co75Ni25.dat")
+d4 <- read.table("AD Co.dat")
 
-data <- bind_cols(d1, d2$V2, d3$V2)
+# TODO: ADD Ni REF DATA AND SUBSAMPLE IT TO FIT
 
-colnames(data) <- c("2_theta", "Ni75Co25", "Ni50Co50", "Ni25Co75")
+data <- bind_cols(d1, d2$V2, d3$V2, d4$V2)
+
+colnames(data) <- c("2_theta", "Ni75Co25", "Ni50Co50", "Ni25Co75", "Co")
 
 ggplot(data, aes(x = `2_theta`)) +
   geom_ribbon(aes(ymin = 0, ymax = `Ni25Co75`, fill = "Ni25Co75"),
-    alpha = 0.5, color = "green"
+    alpha = 0.1, color = "green"
   ) +
   geom_ribbon(aes(ymin = 0, ymax = `Ni50Co50`, fill = "Ni50Co50"),
-    alpha = 0.5, color = "red"
+    alpha = 0.1, color = "red"
   ) +
   geom_ribbon(aes(ymin = 0, ymax = `Ni75Co25`, fill = "Ni75Co25"),
-    alpha = 0.5, color = "blue"
+    alpha = 0.1, color = "blue"
+  ) +
+  geom_ribbon(aes(ymin = 0, ymax = `Co`, fill = "Co"),
+    alpha = 0.1, color = "yellow"
   ) +
   labs(
     title = "Ni_(1-x)Co_X",
@@ -26,27 +32,13 @@ ggplot(data, aes(x = `2_theta`)) +
   ) +
   scale_fill_manual(values = c(
     "Ni75Co25" = "blue", "Ni50Co50" = "red",
-    "Ni25Co75" = "green"
+    "Ni25Co75" = "green", "Co" = "yellow"
   )) +
   theme_minimal()
 
 # Threshold for peaks
-threshold <- 50
-
-# Filter data for peaks above the threshold
-peaks_data <- data %>%
-  filter(Ni75Co25 > threshold)
-
-# Plotting peaks as bars
-ggplot(peaks_data, aes(x = `2_theta`, y = Ni75Co25)) +
-  geom_bar(
-    stat = "identity", position = "dodge",
-    color = "black", alpha = 0.7,
-    width = 0.01
-  ) +
-  ggtitle("Peaks Above Threshold in Ni75Co25") +
-  xlab("2_theta") +
-  ylab("Intensity")
+threshold <- 40
+# PERF: FIND BEST THRESHOLD VALUE
 
 find_peaks <- function(data) {
   if (length(data) < 2) {
@@ -69,20 +61,35 @@ find_peaks <- function(data) {
   return(peaks)
 }
 
-peaks_values <- find_peaks(peaks_data$Ni75Co25)
-print(peaks_values)
-
 data_peaks <- data.frame(
-  x = peaks_data$"2_theta",
-  y1 = peaks_data$Ni75Co25,  # Curve data
-  y2 = peaks_values  # Point data 1
+  x = data$"2_theta",
+  y1 = find_peaks(data$Ni75Co25), # Curve data
+  y2 = find_peaks(data$Ni50Co50), # Point data 1
+  y3 = find_peaks(data$Ni25Co75),
+  y4 = find_peaks(data$Co)
 )
 
-ggplot(data_peaks, aes(x = x)) +
-  geom_line(aes(y = y1), color = "blue") +  # Curve
-  geom_point(aes(y = y2), color = "red") +  # Point series 1
-  ggtitle("Peaks Ni75Co25") +
-  xlab("2_theta") +
-  ylab("Intensity")
+# Filter all the data points with a certain threshold
+data_peaks[, 2:5] <- data_peaks[, 2:5] * (data_peaks[, 2:5] > threshold)
 
-# TODO : DO THIS BUT FOR EVERY CURVE DATA AUTOMATICALLY WITH `sapply` 
+colnames(data_peaks) <- c("2_theta", "Ni75Co25", "Ni50Co50", "Ni25Co75", "Co")
+
+# Create a long-format data frame for ggplot
+data_long <- pivot_longer(data_peaks,
+  cols = -`2_theta`,
+  names_to = "Composition", values_to = "Intensity"
+)
+
+# Plot using ggplot2 with geom_tile
+ggplot(data_long, aes(x = `2_theta`, y = Composition, fill = Intensity)) +
+  geom_tile(color = "white") +
+  scale_fill_gradient(low = "white", high = "blue") +
+  labs(
+    title = "Heatmap of Non-Zero Values",
+    x = "2_theta",
+    y = "Composition",
+    fill = "Intensity"
+  ) +
+  theme_minimal()
+
+# TODO: FIND CLUSTER OF PEAKS TO ANALYSE THEM
